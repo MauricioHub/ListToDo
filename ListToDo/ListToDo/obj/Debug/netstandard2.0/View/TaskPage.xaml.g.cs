4ebbc3dab71aedb22fd6702ea74ee7e46b6f9a41
mauricioBit <mguzman@apsposus.com>
2021-12-21 17:25:03 -0500

    last changes.

--- ListToDo/ListToDo/obj/Debug/netstandard2.0/View/TaskPage.xaml.g.cs
+++ ListToDo/ListToDo/obj/Debug/netstandard2.0/View/TaskPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace ListToDo.View {
     public partial class TaskPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.ListView LvPersonas;
+        private global::Xamarin.Forms.ListView LvTask;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TaskPage));
-            LvPersonas = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "LvPersonas");
+            LvTask = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "LvTask");
         }
     }
 }
